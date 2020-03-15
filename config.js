47ccd103304eb7546f8d0a800e78a65990024f3b
Sazz <39680458+Sazzo@users.noreply.github.com>
2020-03-15 14:04:48 -0300

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ module.exports = {
 		Which one you use is ultimately up to you.
 	*/
 	serveFilesWithNode: true,
-	domain: 'https://i.sazz.xyz',
+	domain: 'https://owo.sazz.xyz',
 
 	// Port on which to run the server
 	port: 9999,
